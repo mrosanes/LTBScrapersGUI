7e1e965db899f50aca215485ff060bbc5c640f83
gcuni <gcuni@a18efa20-c439-0410-bf20-9b91ec695b4c>
2008-07-09 13:00:52 +0000

    increment and decrement the stepper position
    git-svn-id: file:///siciliarep/svn/trunk/tango_client/LTBScrapersGUI@4303 a18efa20-c439-0410-bf20-9b91ec695b4c

--- scrapers_rc.py
+++ scrapers_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Tue Jun 17 14:40:25 2008
+# Created: Wed Jul 9 12:11:41 2008
 #      by: The Resource Compiler for PyQt (Qt v4.3.4)
 #
 # WARNING! All changes made in this file will be lost!
