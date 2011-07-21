a7ad296af75b2a500bbaf086cd605b3bc4a2415b
gcuni <gcuni@a18efa20-c439-0410-bf20-9b91ec695b4c>
2011-07-21 14:24:37 +0000

    new version of scrapers gui using standard taurusform + logo + title + abort button
    git-svn-id: file:///siciliarep/svn/trunk/tango_client/LTBScrapersGUI@10416 a18efa20-c439-0410-bf20-9b91ec695b4c

--- ctdiscrapers.py
+++ ctdiscrapers.py
@@ -1,151 +1,154 @@
 #!/usr/bin/env python
 import sys
 from PyQt4 import QtGui,QtCore
-from ui_scrh import Ui_SCRH
-from ui_scrv import Ui_SCRV
-from scrapercontroller import ScraperController
 
-class SCRH(QtGui.QMainWindow,ScraperController):
-    
+from ui_scrapersform import Ui_ScraperForm
+import taurus
+
+class ScraperWindow(QtGui.QMainWindow):
     def __init__(self, models, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
 
-        # Get graphical information
-        self.ui = Ui_SCRH()
-        self.ui.setupUi(self)
+        self.w = QtGui.QWidget()
+        self.ui = Ui_ScraperForm()
+        self.ui.setupUi(self.w)
+
+        self.setCentralWidget(self.w)    
 
         self.scraper_name = models['SCRAPER_NAME']
         self.scraper_name_tooltip = models['SCRAPER_NAME_TOOLTIP']
-        self.left_stepper = models['LEFT_STEPPER']
-        self.right_stepper = models['RIGHT_STEPPER']
-        self.left_enc = models['LEFT_ENC']
-        self.right_enc = models['RIGHT_ENC']
-        self.gap = models['GAP']
-        self.offset = models['OFFSET']
 
-        # The scraper controller functionality needs the UI components
-        ScraperController.__init__(
-            self,self.left_enc,self.right_enc
-            ,self.ui.abort
-            ,self.ui.leftInValue,self.ui.leftMoveIn
-            ,self.ui.leftOutValue,self.ui.leftMoveOut
-            ,self.ui.leftAbsValue,self.ui.leftMoveAbs
-            ,self.ui.rightInValue,self.ui.rightMoveIn
-            ,self.ui.rightOutValue,self.ui.rightMoveOut
-            ,self.ui.rightAbsValue,self.ui.rightMoveAbs)
+        self.first_motor_name = models['FIRST_MOTOR']
+        self.second_motor_name = models['SECOND_MOTOR']
+        self.gap_motor_name = models['GAP']
+        self.offset_motor_name = models['OFFSET']
+
+        self.label_names = models['MOTOR_LABEL_NAMES']
+
+        self.first_motor = taurus.Device(self.first_motor_name)
+        self.second_motor = taurus.Device(self.second_motor_name)
+        self.gap_motor = taurus.Device(self.gap_motor_name)
+        self.offset_motor = taurus.Device(self.offset_motor_name)
+
+        self.connect(self.ui.abort, QtCore.SIGNAL("clicked()"), self.abort)
+
+        self.form_models = [self.first_motor_name, self.second_motor_name, self.gap_motor_name, self.offset_motor_name]
+        if models.has_key('FORM_EXTRA_MODELS'):
+            self.form_models += models['FORM_EXTRA_MODELS']
 
         self.setTextAndModels()
+
+        ### Set some PoolMotorSlim controls visible
+        ### Ubaldo also requests to change labels to something more intuitive: Upper/Lower Jaw, and Left/Right Jaw
+        for motor_name in [self.first_motor_name, self.second_motor_name, self.gap_motor_name, self.offset_motor_name]:
+            motor_tv_widget = self.ui.form.getItemByModel(motor_name)
+            motor_pms = motor_tv_widget.customWidget()
+            motor_pms.toggleHideAll()
+            motor_pms.toggleMoveAbsolute(True)
+            motor_pms.toggleMoveRelative(True)
+
+            pms_label = motor_pms.taurus_value.labelWidget()
+            pms_label.setFgRole('none')
+            pms_label.setPrefixText(self.label_names[motor_name])
+
+    def sizeHint(self):
+        return QtCore.QSize(650,500)
         
     def setTextAndModels(self):
         # Scraper name
-        self.ui.SCRHName.setText(self.scraper_name)
-        self.ui.SCRHName.setToolTip(self.scraper_name_tooltip)
+        self.ui.scraperName.setText(self.scraper_name)
+        self.ui.scraperName.setToolTip(self.scraper_name_tooltip)
         
-        # Real Motors
-        self.ui.leftStepper.setModel(self.left_stepper)
-        self.ui.rightStepper.setModel(self.right_stepper)
-        
-        # Pseudo Motors:
-        self.ui.leftEnc.setModel(self.left_enc)
-        self.ui.rightEnc.setModel(self.right_enc)
-        self.ui.gap.setModel(self.gap)
-        self.ui.offset.setModel(self.offset)
+        # For Pool motors, use the PoolMotorSlim widget
+        from taurus.qt.qtgui.extra_pool import PoolMotorSlim
+        self.ui.form.setCustomWidgetMap({'SimuMotor':PoolMotorSlim,
+                                         'Motor':PoolMotorSlim,
+                                         'PseudoMotor':PoolMotorSlim})
+        self.ui.form.setModel(self.form_models)
 
-class SCRV(QtGui.QMainWindow,ScraperController):
-    
-    def __init__(self, models, parent=None):
-        QtGui.QMainWindow.__init__(self, parent)
+    def abort(self):
+        exception_infos = []
+        for motor in [self.first_motor, self.second_motor, self.gap_motor, self.offset_motor]:
+            try:
+                motor.abort()
+            except:
+                exception_infos.append(sys.exc_info())
 
-        # Get graphical information
-        self.ui = Ui_SCRV()
-        self.ui.setupUi(self)
-
-        self.scraper_name = models['SCRAPER_NAME']
-        self.scraper_name_tooltip = models['SCRAPER_NAME_TOOLTIP']
-        self.upper_stepper = models['UPPER_STEPPER']
-        self.lower_stepper = models['LOWER_STEPPER']
-        self.upper_enc = models['UPPER_ENC']
-        self.lower_enc = models['LOWER_ENC']
-        self.gap = models['GAP']
-        self.offset = models['OFFSET']
-
-        # The scraper controller functionality needs the UI components
-        ScraperController.__init__(
-            self,self.upper_enc,self.lower_enc
-            ,self.ui.abort
-            ,self.ui.upperInValue,self.ui.upperMoveIn
-            ,self.ui.upperOutValue,self.ui.upperMoveOut
-            ,self.ui.upperAbsValue,self.ui.upperMoveAbs
-            ,self.ui.lowerInValue,self.ui.lowerMoveIn
-            ,self.ui.lowerOutValue,self.ui.lowerMoveOut
-            ,self.ui.lowerAbsValue,self.ui.lowerMoveAbs)
-
-        self.setTextAndModels()
-        
-    def setTextAndModels(self):
-        # Scraper name
-        self.ui.SCRVName.setText(self.scraper_name)
-        self.ui.SCRVName.setToolTip(self.scraper_name_tooltip)
-        
-        # Real Motors
-        self.ui.upperStepper.setModel(self.upper_stepper)
-        self.ui.lowerStepper.setModel(self.lower_stepper)
-        
-        # Pseudo Motors:
-        self.ui.upperEnc.setModel(self.upper_enc)
-        self.ui.lowerEnc.setModel(self.lower_enc)
-        self.ui.gap.setModel(self.gap)
-        self.ui.offset.setModel(self.offset)
+        if len(exception_infos) > 0:
+            print 'oups, some exceptions aborting motions'
+            print exception_infos
 
 LT02_SCRH_MODELS = {}
 LT02_SCRH_MODELS['SCRAPER_NAME'] = 'Horizontal Scraper - Diagnostics Line'
 LT02_SCRH_MODELS['SCRAPER_NAME_TOOLTIP'] = 'LT-DI-SCRH-T0201'
-LT02_SCRH_MODELS['LEFT_STEPPER'] = 'motor/lt_ipapscrapers_ctrl/1'
-LT02_SCRH_MODELS['RIGHT_STEPPER'] = 'motor/lt_ipapscrapers_ctrl/2'
-LT02_SCRH_MODELS['LEFT_ENC'] = LT02_SCRH_MODELS['LEFT_STEPPER']
-LT02_SCRH_MODELS['RIGHT_ENC'] = LT02_SCRH_MODELS['RIGHT_STEPPER']
+LT02_SCRH_MODELS['FIRST_MOTOR'] = 'motor/lt_ipapscrapers_ctrl/1'
+LT02_SCRH_MODELS['SECOND_MOTOR'] = 'motor/lt_ipapscrapers_ctrl/2'
 LT02_SCRH_MODELS['GAP'] = 'pm/lt02_hslit_ctrl/1'
 LT02_SCRH_MODELS['OFFSET'] = 'pm/lt02_hslit_ctrl/2'
+LT02_SCRH_MODELS['MOTOR_LABEL_NAMES'] = {'motor/lt_ipapscrapers_ctrl/1':'Left Jaw',
+                                         'motor/lt_ipapscrapers_ctrl/2':'Right Jaw',
+                                         'pm/lt02_hslit_ctrl/1':'Gap',
+                                         'pm/lt02_hslit_ctrl/2':'Offset'}
 
 LT01_SCRV_MODELS = {}
 LT01_SCRV_MODELS['SCRAPER_NAME'] = 'Vertical Scraper\nLinac To Booster Line'
 LT01_SCRV_MODELS['SCRAPER_NAME_TOOLTIP'] = 'LT-DI-SCRV-T0101'
-LT01_SCRV_MODELS['UPPER_STEPPER'] = 'motor/lt_ipapscrapers_ctrl/3'
-LT01_SCRV_MODELS['LOWER_STEPPER'] = 'motor/lt_ipapscrapers_ctrl/4'
-LT01_SCRV_MODELS['UPPER_ENC'] = LT01_SCRV_MODELS['UPPER_STEPPER']
-LT01_SCRV_MODELS['LOWER_ENC'] = LT01_SCRV_MODELS['LOWER_STEPPER']
+LT01_SCRV_MODELS['FIRST_MOTOR'] = 'motor/lt_ipapscrapers_ctrl/3'
+LT01_SCRV_MODELS['SECOND_MOTOR'] = 'motor/lt_ipapscrapers_ctrl/4'
 LT01_SCRV_MODELS['GAP'] = 'pm/lt01_vslit_ctrl/1'
 LT01_SCRV_MODELS['OFFSET'] = 'pm/lt01_vslit_ctrl/2'
+LT01_SCRV_MODELS['MOTOR_LABEL_NAMES'] = {'motor/lt_ipapscrapers_ctrl/3':'Upper Jaw',
+                                         'motor/lt_ipapscrapers_ctrl/4':'Lower Jaw',
+                                         'pm/lt01_vslit_ctrl/1':'Gap',
+                                         'pm/lt01_vslit_ctrl/2':'Offset'}
 
 LT01_SCRH_MODELS = {}
 LT01_SCRH_MODELS['SCRAPER_NAME'] = 'Horizontal Scraper\nLinac To Booster Line'
 LT01_SCRH_MODELS['SCRAPER_NAME_TOOLTIP'] = 'LT-DI-SCRH-T0101'
-LT01_SCRH_MODELS['LEFT_STEPPER'] = 'motor/lt_ipapscrapers_ctrl/5'
-LT01_SCRH_MODELS['RIGHT_STEPPER'] = 'motor/lt_ipapscrapers_ctrl/6'
-LT01_SCRH_MODELS['LEFT_ENC'] = LT01_SCRH_MODELS['LEFT_STEPPER']
-LT01_SCRH_MODELS['RIGHT_ENC'] = LT01_SCRH_MODELS['RIGHT_STEPPER']
+LT01_SCRH_MODELS['FIRST_MOTOR'] = 'motor/lt_ipapscrapers_ctrl/5'
+LT01_SCRH_MODELS['SECOND_MOTOR'] = 'motor/lt_ipapscrapers_ctrl/6'
 LT01_SCRH_MODELS['GAP'] = 'pm/lt01_hslit_ctrl/1'
 LT01_SCRH_MODELS['OFFSET'] = 'pm/lt01_hslit_ctrl/2'
+LT01_SCRH_MODELS['MOTOR_LABEL_NAMES'] = {'motor/lt_ipapscrapers_ctrl/5':'Left Jaw',
+                                         'motor/lt_ipapscrapers_ctrl/6':'Right Jaw',
+                                         'pm/lt01_hslit_ctrl/1':'Gap',
+                                         'pm/lt01_hslit_ctrl/2':'Offset'}
 
 SR_SCRV_MODELS = {}
 SR_SCRV_MODELS['SCRAPER_NAME'] = 'Vertical Scraper\nInjection Straight'
 SR_SCRV_MODELS['SCRAPER_NAME_TOOLTIP'] = 'SR-DI-SCRV-S16-01'
-SR_SCRV_MODELS['UPPER_STEPPER'] = 'motor/sr_ipapfshscrapers_ctrl/3'
-SR_SCRV_MODELS['LOWER_STEPPER'] = 'motor/sr_ipapfshscrapers_ctrl/4'
-SR_SCRV_MODELS['UPPER_ENC'] = SR_SCRV_MODELS['UPPER_STEPPER']
-SR_SCRV_MODELS['LOWER_ENC'] = SR_SCRV_MODELS['LOWER_STEPPER']
+SR_SCRV_MODELS['FIRST_MOTOR'] = 'motor/sr_ipapfshscrapers_ctrl/3'
+SR_SCRV_MODELS['SECOND_MOTOR'] = 'motor/sr_ipapfshscrapers_ctrl/4'
 SR_SCRV_MODELS['GAP'] = 'pm/sr_vslit_ctrl/1'
 SR_SCRV_MODELS['OFFSET'] = 'pm/sr_vslit_ctrl/2'
+SR_SCRV_MODELS['MOTOR_LABEL_NAMES'] = {'motor/sr_ipapfshscrapers_ctrl/3':'Upper Jaw',
+                                       'motor/sr_ipapfshscrapers_ctrl/4':'Lower Jaw',
+                                       'pm/sr_vslit_ctrl/1':'Gap',
+                                       'pm/sr_vslit_ctrl/2':'Offset'}
+SR_SCRV_MODELS['FORM_EXTRA_MODELS'] = ['sr16/ct/scrv-plc-01/Tin','sr16/ct/scrv-plc-01/Tup','sr16/ct/scrv-plc-01/Tdown','sr16/ct/scrv-plc-01/FM_VECA']
 
 SR_SCRH_MODELS = {}
 SR_SCRH_MODELS['SCRAPER_NAME'] = 'Horizontal Scraper\nInjection Straight'
 SR_SCRH_MODELS['SCRAPER_NAME_TOOLTIP'] = 'SR-DI-SCRH-S16-01'
-SR_SCRH_MODELS['LEFT_STEPPER'] = 'motor/sr_ipapfshscrapers_ctrl/5'
-SR_SCRH_MODELS['RIGHT_STEPPER'] = 'motor/sr_ipapfshscrapers_ctrl/6'
-SR_SCRH_MODELS['LEFT_ENC'] = SR_SCRH_MODELS['LEFT_STEPPER']
-SR_SCRH_MODELS['RIGHT_ENC'] = SR_SCRH_MODELS['RIGHT_STEPPER']
+SR_SCRH_MODELS['FIRST_MOTOR'] = 'motor/sr_ipapfshscrapers_ctrl/5'
+SR_SCRH_MODELS['SECOND_MOTOR'] = 'motor/sr_ipapfshscrapers_ctrl/6'
 SR_SCRH_MODELS['GAP'] = 'pm/sr_hslit_ctrl/1'
 SR_SCRH_MODELS['OFFSET'] = 'pm/sr_hslit_ctrl/2'
+SR_SCRH_MODELS['MOTOR_LABEL_NAMES'] = {'motor/sr_ipapfshscrapers_ctrl/5':'Left Jaw',
+                                       'motor/sr_ipapfshscrapers_ctrl/6':'Right Jaw',
+                                       'pm/sr_hslit_ctrl/1':'Gap',
+                                       'pm/sr_hslit_ctrl/2':'Offset'}
+SR_SCRH_MODELS['FORM_EXTRA_MODELS'] = ['sr16/ct/scrv-plc-01/Tin','sr16/ct/scrv-plc-01/Tleft','sr16/ct/scrv-plc-01/Tright','sr16/ct/scrv-plc-01/FM_HOCA']
+
+TEST_MODELS = {}
+TEST_MODELS['SCRAPER_NAME'] = 'Test scraper name\nTest description'
+TEST_MODELS['SCRAPER_NAME_TOOLTIP'] = 'Just a description of the device'
+TEST_MODELS['FIRST_MOTOR'] = 'gc_ipap1'
+TEST_MODELS['SECOND_MOTOR'] = 'gc_ipap2'
+TEST_MODELS['GAP'] = 'gc_hgap'
+TEST_MODELS['OFFSET'] = 'gc_hoffset'
+TEST_MODELS['MOTOR_LABEL_NAMES'] = {'gc_ipap1':'Upper Jaw', 'gc_ipap2':'Lower Jaw', 'gc_hgap':'Gap', 'gc_hoffset':'Offset'}
+TEST_MODELS['FORM_EXTRA_MODELS'] = ['gc_ipap1/dialposition','gc_ipap2/dialposition','gc_hgap/position','gc_hoffset/position']
 
 if __name__ == "__main__":
     usage = '\n\nUse as argument one of the following scrapers:\n\t\tlt02scrh lt01scrv lt01scrh srscrv or srscrh\n'
@@ -157,15 +160,17 @@ if __name__ == "__main__":
 
     scraper = sys.argv[1]
     if scraper.upper() == 'LT02SCRH':
-        window = SCRH(LT02_SCRH_MODELS)
+        window = ScraperWindow(LT02_SCRH_MODELS)
     elif scraper.upper() == 'LT01SCRH':
-        window = SCRH(LT01_SCRH_MODELS)
+        window = ScraperWindow(LT01_SCRH_MODELS)
     elif scraper.upper() == 'LT01SCRV':
-        window = SCRV(LT01_SCRV_MODELS)
+        window = ScraperWindow(LT01_SCRV_MODELS)
     elif scraper.upper() == 'SRSCRH':
-        window = SCRH(SR_SCRH_MODELS)
+        window = ScraperWindow(SR_SCRH_MODELS)
     elif scraper.upper() == 'SRSCRV':
-        window = SCRV(SR_SCRV_MODELS)
+        window = ScraperWindow(SR_SCRV_MODELS)
+    elif scraper.upper() == 'TEST':
+        window = ScraperWindow(TEST_MODELS)
     else:
         print usage
         sys.exit(-1)
