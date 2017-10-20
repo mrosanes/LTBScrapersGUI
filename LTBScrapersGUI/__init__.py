afef2a8d5ce2e5a860cab7614226e35f06ffaf6a
mrosanes <mrosanes@cells.es>
2017-10-20 16:07:06 +0200

    Update the License notice

--- LTBScrapersGUI/__init__.py
+++ LTBScrapersGUI/__init__.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
 
-# ##### BEGIN GPL LICENSE BLOCK #####
+###############################################################################
 #
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 3
-#  of the License, or (at your option) any later version.
+# This file is part of the Linac To Booster Scrapers GUI
 #
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+# Copyright (C) 2008 CELLS / ALBA Synchrotron, Bellaterra, Spain
+# Author: Guifré Cuní
 #
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# ##### END GPL LICENSE BLOCK #####
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+###############################################################################
+
 
 __author__ = "Guifré Cuní"
 __copyright__ = "Copyright 2008, CELLS / ALBA Synchrotron"
