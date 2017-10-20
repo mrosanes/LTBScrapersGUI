799b2bcb00bff0e290f6a6238a656029a8c84a47
mrosanes <mrosanes@cells.es>
2017-10-20 15:43:14 +0200

    Reorganize code to begin adaptation to setuptools
    Reorganize code to allow the beginning of the adaptation to
    the usage of setuptools.

--- ui/makepy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm ../ui_*py
-
-/usr/bin/pyrcc4 scrapers.qrc -o ../scrapers_rc.py
-/usr/bin/pyuic4 SCRH.ui -o ../ui_scrh.py
-/usr/bin/pyuic4 SCRV.ui -o ../ui_scrv.py
-
-taurusuic4 -x -o ../ui_scrapersform.py scrapersform.ui
